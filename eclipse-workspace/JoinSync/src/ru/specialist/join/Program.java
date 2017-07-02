b02d8b7740e13a14a52ea34276823eba41d1675a
TutunnikovaNathalie <initgraf@mail.ru>
2017-07-02 16:56:15 +0300

    threads

---
+++ eclipse-workspace/JoinSync/src/ru/specialist/join/Program.java
@@ -0,0 +1,39 @@
+package ru.specialist.join;
+
+import static java.lang.System.out;
+public class Program {
+
+	public static void main(String[] args) throws InterruptedException {
+		Thread t1, t2;
+		Thread main = Thread.currentThread();
+		
+		(t1 = new Thread(()-> {
+			for(int i=1; i <= 100; i++)
+			{
+				if (i==50) main.interrupt();
+				out.printf("%s : %d\n",Thread.currentThread().getName(), i);
+			}
+		}, "A")).start();
+		
+		(t2 = new Thread(()-> {
+			for(int i=1; i <= 100; i++)
+				out.printf("%s : %d\n",Thread.currentThread().getName(), i);	
+		}, "B")).start();
+		
+		//Thread.sleep(1000);
+		try
+		{
+			t1.join();
+		} catch(InterruptedException ex)
+		{
+			out.println("ушат холодной воды");
+			out.println(main.isInterrupted());
+			out.println(Thread.interrupted());
+		}
+		t2.join();
+		
+		out.println(Thread.currentThread().getName()); 
+		
+	}
+
+}
---
+++ eclipse-workspace/ProcessStart/src/ru/specialist/processstart/Program.java
@@ -0,0 +1,29 @@
+package ru.specialist.processstart;
+
+import java.io.*;
+import java.nio.charset.Charset;
+
+public class Program {
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+		System.out.println("Process start...");
+		
+		File cd = new File(".");
+		System.out.println(cd.getCanonicalPath());
+		
+		Process p = Runtime.getRuntime().exec("cmd /C dir c:\\");
+		InputStream in = p.getInputStream();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(in, Charset.forName("cp866")));
+		String s;
+		while((s= reader.readLine())!=null)
+		{
+			System.out.println(s);
+		}
+		
+		int exitCode = p.waitFor();
+		System.out.printf("Exit code: %d\n", exitCode);
+		System.exit(exitCode);
+			
+	}
+
+}
